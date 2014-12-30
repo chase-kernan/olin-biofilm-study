10aee498de0b0810e60cf5c5dc866b94696688ac
Chase Kernan <chase.kernan@gmail.com>
2014-12-30 10:20:27 -0700

    Parse experiments from plan.json files

--- model/experiments.py
+++ model/experiments.py
@@ -1,23 +1,45 @@
-
 from pathlib import Path
 from itertools import product
-from collections import Iterable
+from collections import Iterable, Mapping
 
+import json
 import pandas as pd
+import numpy as np
 
-class Experiment:
+def from_plan(path):
+    def parse_value(value):
+        try:
+            return np.arange(value['from'], value['to'], value['num'])
+        except TypeError:
+            return value
 
-    def __init__(self, name, specs):
-        self.name = name
-        self.specs_frame = pd.DataFrame(specs)
+    def parse_json_plan(json_plan):
+        plan = json_plan.copy()
+        plan['specs'] = { param: parse_value(value) 
+                          for param, value in plan['specs'].items() }
+        return plan
 
-    def data_dir(self, parent_dir):
-        return parent_dir / self.name
+    with path.open('r') as json_file:
+        return Experiment(**parse_json_plan(json.load(json_file)))
+
 
 def cartesian_product(**params):
-    params = {name: value if isinstance(value, Iterable) else [value] 
-              for name, value in params.items()}
+    params = { name: value if isinstance(value, Iterable) else [value] 
+               for name, value in params.items() }
     return map(dict, product(*([(name, value) for value in values]
                                for name, values in params.items())))
 
 
+class Experiment:
+
+    def __init__(self, name="experiment", runs_per_spec=5, specs={}):
+        self.name = name
+        self.runs_per_spec = runs_per_spec
+        self.specs = specs
+
+    @property
+    def expanded_specs(self):
+        return pd.DataFrame(list(cartesian_product(**self.specs)))
+
+    def data_file(self, parent_dir):
+        return parent_dir / "{}.hdf5".format(self.name)
--- test/model/test_experiments.py
+++ test/model/test_experiments.py
@@ -1,29 +1,52 @@
-from model.experiments import Experiment, cartesian_product
 from pathlib import Path
+
 import pandas as pd
+import numpy as np
+
+from model.experiments import Experiment, from_plan
+
+def test_from_plan(tmpdir):
+    path = tmpdir / 'plan.json'
+    with path.open('w') as json_file:
+        json_file.write("""{
+            "name": "test_experiment",
+            "runs_per_spec": 3,
+            "specs": {
+                "a": 1,
+                "b": [3, 4],
+                "c": { "from": 1, "to": 10, "num": 5 }
+            }
+        }""")
+
+    exp = from_plan(path)
+    assert exp.name == 'test_experiment'
+    assert exp.runs_per_spec == 3
+    assert exp.specs['a'] == 1
+    assert exp.specs['b'] == [3, 4]
+    assert all(exp.specs['c'] == np.arange(1, 10, 5))
 
 def test_new_experiment():
-    exp = Experiment('my experiment', [])
-    assert exp.name == 'my experiment'
+    specs = { 'a': 10 }
+    exp = Experiment('my_experiment', runs_per_spec=9, specs=specs)
+    assert exp.name == 'my_experiment'
+    assert exp.runs_per_spec == 9
+    assert exp.specs == specs
 
-def test_data_dir():
-    exp = Experiment('some-exp', [])
+def test_data_file():
+    exp = Experiment('some-exp')
     root = Path('root')
-    assert exp.data_dir(root) == root/'some-exp'
+    assert exp.data_file(root) == root/'some-exp.hdf5'
 
-def test_empty_specs_frame():
-    exp = Experiment('some-exp', [])
-    assert exp.specs_frame.empty
-
-def test_cartesian_product():
-    actual = cartesian_product(
+def test_expanded_specs():
+    expanded = Experiment(specs=dict(
         a=1,
         b=[3, 4, 5],
         c=[6, 7],
-    )
+    )).expanded_specs
 
     keys = 'a', 'b', 'c'
-    as_tuples = {tuple(value[key] for key in keys) for value in actual}
+    as_tuples = { tuple(value[key] for key in keys) 
+                  for _, value in expanded.iterrows() }
     assert as_tuples == {
         (1, 3, 6),
         (1, 3, 7),
