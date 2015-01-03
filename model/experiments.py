b5e893e9680ae155e2962c9652e2b1ab409a1161
Chase Kernan <chase.kernan@gmail.com>
2015-01-02 17:24:28 -0800

    Refactor plans into steps, using .py plan files

--- model/experiments.py
+++ model/experiments.py
@@ -6,39 +6,83 @@ import json
 import pandas as pd
 import numpy as np
 
+STEPS = set()
+
+def register_step(step):
+    STEPS.add(step)
+
+
 def load_plan(path):
-    with path.open('r') as json_file:
-        return Plan.from_json(json.load(json_file))
+    plan = []
+    exposed = {
+        'Plan': lambda *args, **kwargs: plan.append(Plan(*args, **kwargs)),
+        'np': np,
+        'pd': pd }
+    exposed.update({ cls.__name__: cls for cls in STEPS })
+
+    with path.open('r') as plan_file:
+        exec(plan_file.read(), exposed)
+
+    return plan[0]
+
 
 class Plan:
-    @classmethod
-    def from_json(cls, json_plan):
-        def parse_value(value):
-            try:
-                return np.arange(value['from'], value['to'], value['num'])
-            except TypeError:
-                return value
-            
-        return cls(name=json_plan['name'],
-                   runs_per_spec=json_plan['runs_per_spec'],
-                   specs={ param: parse_value(value)
-                           for param, value in json_plan['specs'].items() })
-
-    def __init__(self, name="experiment", runs_per_spec=5, specs={}):
+    def __init__(self, name="experiment", steps=None):
         self.name = name
-        self.runs_per_spec = runs_per_spec
-        self.specs = specs
+        self.steps = steps or []
 
-    @property
-    def expanded_specs(self):
-        return pd.DataFrame(list(_cartesian_product(**self.specs)))
 
     def experiment_file(self, parent_dir):
         return parent_dir / "{}.hdf5".format(self.name)
 
+    def run(self, parent_dir):
+        store = pd.HDFStore(self.experiment_file(parent_dir))
+        for step in self.steps:
+            if not step.complete(store):
+                step.run(store)
+        return store
 
-def _cartesian_product(**params):
-    params = { name: value if isinstance(value, Iterable) else [value] 
-               for name, value in params.items() }
-    return map(dict, product(*([(name, value) for value in values]
-                               for name, values in params.items())))
+
+class CreateSpecs:
+    def __init__(self, **params):
+        self.params = params
+
+    @property
+    def expanded_specs(self):
+        params = { name: value if isinstance(value, Iterable) else [value] 
+                   for name, value in self.params.items() }
+
+        return pd.DataFrame(list(map(dict, 
+            product(*([(name, value) for value in values]
+                       for name, values in params.items())))))
+
+    def complete(self, store):
+        return 'specs' in store
+
+    def run(self, store):
+        store['specs'] = self.expanded_specs
+register_step(CreateSpecs)
+
+
+class RunModels:
+    def __init__(self, runs_per_spec=5):
+        self.runs_per_spec = runs_per_spec
+
+    def complete(self, store):
+        return False
+    
+    def run(self, store):
+        pass
+register_step(RunModels)
+
+
+class AnalyzeModels:
+    def __init__(self, fields=None):
+        self.fields = fields or []
+
+    def complete(self, store):
+        return False
+    
+    def run(self, store):
+        pass
+register_step(AnalyzeModels)
--- test/model/test_experiments.py
+++ test/model/test_experiments.py
@@ -3,46 +3,63 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
-from model.experiments import Plan, load_plan
+from model.experiments import Plan, load_plan, \
+                              CreateSpecs, RunModels, AnalyzeModels
 
-def test_load_plan(tmpdir):
-    path = tmpdir / 'plan.json'
-    with path.open('w') as json_file:
-        json_file.write("""{
-            "name": "test_experiment",
-            "runs_per_spec": 3,
-            "specs": {
-                "a": 1,
-                "b": [3, 4],
-                "c": { "from": 1, "to": 10, "num": 5 }
-            }
-        }""")
+def load_plan_str(tmpdir, plan_str):
+    path = tmpdir/'plan.py'
+    with path.open('w') as plan_file:
+        plan_file.write(plan_str)
+    return load_plan(path)
 
-    plan = load_plan(path)
-    assert plan.name == 'test_experiment'
-    assert plan.runs_per_spec == 3
-    assert plan.specs['a'] == 1
-    assert plan.specs['b'] == [3, 4]
-    assert all(plan.specs['c'] == np.arange(1, 10, 5))
+
+def test_load_plan_name(tmpdir):
+    plan = load_plan_str(tmpdir, 'Plan(name="testing")')
+    assert plan.name == 'testing'
+
+def test_load_plan_globals(tmpdir):
+    assert load_plan_str(tmpdir, 'np; pd; Plan()')
+
+def test_load_plan_with_create_specs(tmpdir):
+    plan = load_plan_str(tmpdir, """
+        Plan(steps=[CreateSpecs(a=1, b=np.arange(0, 1), c=[3, 4])])
+    """.strip())
+    create = plan.steps[0]
+    assert create.params['a'] == 1
+    assert all(create.params['b'] == np.arange(0, 1))
+    assert create.params['c'] == [3, 4]
+
+def test_load_plan_with_run_models(tmpdir):
+    plan = load_plan_str(tmpdir, """
+        Plan(steps=[RunModels(runs_per_spec=9)])
+    """.strip())
+    run = plan.steps[0]
+    assert run.runs_per_spec == 9
+
+def test_load_plan_with_analyze_models(tmpdir):
+    plan = load_plan_str(tmpdir, """
+        Plan(steps=[AnalyzeModels(fields=['a', 'b', 'c'])])
+    """.strip())
+    analyze = plan.steps[0]
+    assert analyze.fields == ['a', 'b', 'c']
 
 def test_new_plan():
-    specs = { 'a': 10 }
-    plan = Plan('my_experiment', runs_per_spec=9, specs=specs)
+    steps = [CreateSpecs(), RunModels()]
+    plan = Plan(name='my_experiment', steps=steps)
     assert plan.name == 'my_experiment'
-    assert plan.runs_per_spec == 9
-    assert plan.specs == specs
+    assert plan.steps == steps
 
 def test_experiment_file():
     exp = Plan('some-exp')
     root = Path('root')
     assert exp.experiment_file(root) == root/'some-exp.hdf5'
 
-def test_expanded_specs():
-    expanded = Plan(specs=dict(
+def test_create_specs_expanded_specs():
+    expanded = CreateSpecs(
         a=1,
         b=[3, 4, 5],
         c=[6, 7],
-    )).expanded_specs
+    ).expanded_specs
 
     keys = 'a', 'b', 'c'
     as_tuples = { tuple(value[key] for key in keys) 
